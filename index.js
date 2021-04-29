7a4cdd0c7c1379292793514eb469986863135831
Juarez Goulart <goulartgarcia12@gmail.com>
2021-04-28 22:08:13 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function animar() {
+    let navi = document.querySelector('.wave-support')
+    let ondaTotal = document.querySelector('.wave1')
+    let ondaTotal2 = document.querySelector('.wave2')
+    let ondaTotal3 = document.querySelector('.wave3')
+    let ondaTotal4 = document.querySelector('.wave4')
+    navi.classList.toggle('active')
+    ondaTotal.classList.toggle('active')
+    ondaTotal2.classList.toggle('active')
+    ondaTotal3.classList.toggle('active')
+    ondaTotal4.classList.toggle('active')
+}
+
