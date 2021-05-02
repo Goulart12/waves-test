1e33ca47992b2015ca9d4302860546450ff95d80
Juarez Goulart <goulartgarcia12@gmail.com>
2021-05-02 11:07:45 -0300

    novas ondas

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function animar() {
     let ondaTotal2 = document.querySelector('.wave2')
     let ondaTotal3 = document.querySelector('.wave3')
     let ondaTotal4 = document.querySelector('.wave4')
+    let ondaBotao = document.querySelector('.liquid')
     navi.classList.toggle('active')
     ondaTotal.classList.toggle('active')
     ondaTotal2.classList.toggle('active')
     ondaTotal3.classList.toggle('active')
     ondaTotal4.classList.toggle('active')
+    ondaBotao.classList.toggle('active')
 }
 
